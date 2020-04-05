4022402aae7de1ae090c80c83e6b8412b2eb4394
Krishnath <shadyanuj24@gmail.com>
2020-04-05 14:53:31 +0530

    added projects

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
+var nextButton = document.getElementById("next-btn");
+var prevButton = document.getElementById('prev-btn');
+var yelpCamp = document.getElementById('yelpcamp');
+var reactApp = document.getElementById('todo');
+
+
+
+
+
+
+
 changeNavbarColor();
+horizontalScroll();
 
 
 function changeNavbarColor(){
@@ -14,5 +26,19 @@ function changeNavbarColor(){
                 'background':'transparent'
             });
         }
+    });
+}
+
+function horizontalScroll(){
+    $(nextButton).on('click',(e)=>{
+
+        $(yelpCamp).toggleClass('active-project');
+        $(reactApp).toggleClass('active-project');
+    });
+    
+    $(prevButton).on('click',(e)=>{
+    
+        $(yelpCamp).toggleClass('active-project');
+        $(reactApp).toggleClass('active-project');
     });
 }
