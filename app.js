d7e8465c33684a12cf104d044bdbeb8901e93ed4
Krishnath <shadyanuj24@gmail.com>
2020-04-11 16:03:02 +0530

    added skills and footer

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var nextButton = document.getElementById("next-btn");
 var prevButton = document.getElementById('prev-btn');
 var yelpCamp = document.getElementById('yelpcamp');
 var reactApp = document.getElementById('todo');
-
-
+var profileImage = document.getElementsByClassName('profile-image');
 
 
 
@@ -13,21 +12,32 @@ changeNavbarColor();
 horizontalScroll();
 
 
+
+
+
+
+
 function changeNavbarColor(){
     $(window).scroll(function () {
         
-        if ($(window).scrollTop() >= 30) {
-        $('header').css({
-            'background':'lightgray'
         
+        if ($(window).scrollTop() >= 50) {
+
+        $('header').css({
+            'background':'lightgray',
+            'z-index' : '2'
         });
+       
         } else{
             $('header').css({
                 'background':'transparent'
             });
+            
         }
     });
 }
+
+
 
 function horizontalScroll(){
     $(nextButton).on('click',(e)=>{
